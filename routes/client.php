9b6a0548a2e86e66b13997695f3733e71b2013a2
Setting <minhbyn00000@gmail.com>
2024-05-31 07:44:15 +0700

    Sửa lại code

--- routes/client.php
+++ routes/client.php
@@ -1,12 +1,2 @@
 <?php 
-// Define routes
-$router->get('/', function()
-{
-    echo "Trang Chủ";
-}
-);
-$router->get('/about', function()
-{
-    echo "Giới Thiệu";
-}
-);
+
--- routes/index.php
+++ routes/index.php
@@ -1,9 +1,1 @@
 <?php
-// Create Router instance
-$router = new \Bramus\Router\Router();
-
-require_once __DIR__ . '/admin.php';
-require_once __DIR__ . '/client.php.php';
-
-// Run it!
-$router->run();
